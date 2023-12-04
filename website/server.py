b21699ac7522e17377c2c55202d045453e4c5d0d
binhow612 <91769295+binhow612@users.noreply.github.com>
2023-12-05 00:33:24 +0700

    Update server.py

--- website/server.py
+++ website/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import login
 import fetch_data
+import add_patient
 
 app = Flask(__name__)
 
@@ -71,14 +72,14 @@ def register_route(message=None):
 def homepage_route():
     return render_template("homepage.html")
 
-@app.route('/fetch_data', methods = ['GET', 'POST'])
-def fetch_data_route():
+@app.route('/fetch_patient', methods = ['GET', 'POST'])
+def fetch_patient_route():
     if (request.method == 'POST'):
         try:
             data = request.form
             code = data['code']
             # Call the main function in fetch_data.py
-            result = fetch_data.fetch_data(code)
+            result = fetch_data.fetch_data(code, type="P")
             if result == "Error 1":
                 message = f"Error 1: No patient found with code {code}"
             elif result == "Error 2":
@@ -92,24 +93,39 @@ def fetch_data_route():
     # For GET requests, simply render the page
     return render_template("fetch_patient_data.html", message='')
 
-@app.route('/add_data', methods = ['GET', 'POST'])
-def add_data_route():
+@app.route('/fetch_doctor', methods = ['GET', 'POST'])
+def fetch_doctor_route():
     if (request.method == 'POST'):
         try:
             data = request.form
             code = data['code']
             # Call the main function in fetch_data.py
-            result = fetch_data.fetch_data(code)
-            if result == "Error 1":
-                message = f"Error 1: No patient found with code {code}"
-            elif result == "Error 2":
-                message = f"Invalid patient type for patient with code {code}"
+            result = fetch_data.fetch_data(code, type="D")
+            if not result:
+                message = f"There is no patients treated or examined by this doctor!"
             else:
-                message = {f'{key}':f'{value}' for key, value in result.items()}
+                message = result
+                
+            return render_template("fetch_doctor_data.html", message=message)
+
+        except Exception as e:
+            return render_template("fetch_doctor_data.html", message=f'Error 1: {str(e)}')
+    # For GET requests, simply render the page
+    return render_template("fetch_doctor_data.html", message='')
+
+@app.route('/add_data', methods = ['GET', 'POST'])
+def add_data_route():
+    if (request.method == 'POST'):
+        try:
+            patient_data = request.form
+            # Call the main function in add_patient.py
+            result = add_patient.add_patient(patient_data)
+            
+            message = result
             return render_template("add_patient.html", message=message)
 
         except Exception as e:
-            return render_template("add_patient.html", message=f'Error 3: {str(e)}')
+            return render_template("add_patient.html", message=f'Error 2: {str(e)}')
     # For GET requests, simply render the page
     return render_template("add_patient.html", message='')
 
